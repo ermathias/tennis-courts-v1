7c204598c966621bb6c6f0f4911b5fe87b8dc872
Juliano_Bonotto <Juliano_bonotto@dellteam.com>
2020-07-08 16:15:32 -0500

    Controllers fixed

--- src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
+++ src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
@@ -3,23 +3,27 @@ package com.tenniscourts.tenniscourts;
 import com.tenniscourts.config.BaseRestController;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
+@RestController
 public class TennisCourtController extends BaseRestController {
 
     private final TennisCourtService tennisCourtService;
 
-    //TODO: implement rest and swagger
+    @PostMapping("/addTennisCourt")
     public ResponseEntity<Void> addTennisCourt(TennisCourtDTO tennisCourtDTO) {
         return ResponseEntity.created(locationByEntity(tennisCourtService.addTennisCourt(tennisCourtDTO).getId())).build();
     }
 
-    //TODO: implement rest and swagger
+    @GetMapping("/findTennisCourtById")
     public ResponseEntity<TennisCourtDTO> findTennisCourtById(Long tennisCourtId) {
         return ResponseEntity.ok(tennisCourtService.findTennisCourtById(tennisCourtId));
     }
 
-    //TODO: implement rest and swagger
+    @GetMapping("/findTennisCourtSchedById")
     public ResponseEntity<TennisCourtDTO> findTennisCourtWithSchedulesById(Long tennisCourtId) {
         return ResponseEntity.ok(tennisCourtService.findTennisCourtWithSchedulesById(tennisCourtId));
     }
