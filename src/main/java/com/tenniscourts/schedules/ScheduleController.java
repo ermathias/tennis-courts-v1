87d9a5672f497565c4faa7e4914d0549c996f9f1
luizbotton <31282962+luizbotton@users.noreply.github.com>
2020-09-04 16:22:09 -0300

    adding controller rest calls and swagger config to Schedule

--- src/main/java/com/tenniscourts/schedules/ScheduleController.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleController.java
@@ -1,15 +1,23 @@
 package com.tenniscourts.schedules;
 
-import com.tenniscourts.config.BaseRestController;
-import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tenniscourts.config.BaseRestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.AllArgsConstructor;
+
 @AllArgsConstructor
+@RestController
 public class ScheduleController extends BaseRestController {
 
     private final ScheduleService scheduleService;
@@ -26,7 +34,19 @@ public class ScheduleController extends BaseRestController {
     }
 
     //TODO: implement rest and swagger
+    @ApiOperation(value = "Returns a schedule searched by id")
+    @ApiResponses(value = {
+    	    @ApiResponse(code = 200, message = "Returns the schedule list")})
     public ResponseEntity<ScheduleDTO> findByScheduleId(Long scheduleId) {
         return ResponseEntity.ok(scheduleService.findSchedule(scheduleId));
+    }
+    
+    //TODO: implement rest and swagger
+    @ApiOperation(value = "Returns a full schedule list")
+    @ApiResponses(value = {
+    	    @ApiResponse(code = 200, message = "Returns the schedule list")})
+    @GetMapping(value = "/schedules", produces = "application/json")
+    public ResponseEntity<List<Schedule>> findSchedules() {
+        return ResponseEntity.ok(scheduleService.findSchedules());
     }
 }
--- src/main/java/com/tenniscourts/schedules/ScheduleService.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleService.java
@@ -3,8 +3,12 @@ package com.tenniscourts.schedules;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import com.tenniscourts.exceptions.EntityNotFoundException;
+import com.tenniscourts.guests.Guest;
+
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -15,21 +19,37 @@ public class ScheduleService {
     private final ScheduleMapper scheduleMapper;
 
     public ScheduleDTO addSchedule(Long tennisCourtId, CreateScheduleRequestDTO createScheduleRequestDTO) {
-        //TODO: implement addSchedule
-        return null;
+        //TODO: implement addSchedule done?
+    	return scheduleMapper.map(scheduleRepository.saveAndFlush(scheduleMapper.map(createScheduleRequestDTO)));
     }
 
     public List<ScheduleDTO> findSchedulesByDates(LocalDateTime startDate, LocalDateTime endDate) {
-        //TODO: implement
-        return null;
+        //TODO: implement done?
+        return scheduleRepository.findAllByStartDateTimeAndEndDateTime(startDate, endDate);
     }
 
     public ScheduleDTO findSchedule(Long scheduleId) {
-        //TODO: implement
-        return null;
+        //TODO: implement done?
+    	return scheduleRepository.findById(scheduleId).map(scheduleMapper::map).<EntityNotFoundException>orElseThrow(() -> {
+    		throw new EntityNotFoundException("Schedule not found.");
+    	});
+    }
+    
+    public Optional<Schedule> findScheduleObject(Long scheduleId) {
+        //TODO: implement done?
+    	return scheduleRepository.findById(scheduleId);
     }
 
     public List<ScheduleDTO> findSchedulesByTennisCourtId(Long tennisCourtId) {
         return scheduleMapper.map(scheduleRepository.findByTennisCourt_IdOrderByStartDateTime(tennisCourtId));
     }
+    
+    public List<Schedule> findSchedules() {
+        //TODO: implement done?
+        return scheduleRepository.findAll();
+    }
+    
+    public Optional<Schedule> findById(Long id) {
+		return scheduleRepository.findById(id);
+	}
 }
