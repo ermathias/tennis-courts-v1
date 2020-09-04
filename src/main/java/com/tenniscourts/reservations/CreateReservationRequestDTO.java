9894ace738820176c8c7482c82d999f1e37eb643
luizbotton <31282962+luizbotton@users.noreply.github.com>
2020-09-04 16:23:38 -0300

    Value field needed (constraint)

--- src/main/java/com/tenniscourts/reservations/CreateReservationRequestDTO.java
+++ src/main/java/com/tenniscourts/reservations/CreateReservationRequestDTO.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
@@ -22,5 +24,8 @@ public class CreateReservationRequestDTO {
 
     @NotNull
     private Long scheduleId;
+    
+    @NotNull
+    private BigDecimal value;
 
 }
