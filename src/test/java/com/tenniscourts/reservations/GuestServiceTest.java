e895f99dc55c8077cbfbd26f12d241b2d9db8fca
luizbotton <31282962+luizbotton@users.noreply.github.com>
2020-09-04 16:37:49 -0300

    Guest objects included, rest calls and swagger on Schedules, Reservations and TennisCourts

---
+++ src/test/java/com/tenniscourts/reservations/GuestServiceTest.java
@@ -0,0 +1,38 @@
+package com.tenniscourts.reservations;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.tenniscourts.guests.GuestService;
+import com.tenniscourts.schedules.Schedule;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration(classes = ReservationService.class)
+public class GuestServiceTest {
+
+    @InjectMocks
+    GuestService guestService;
+
+    @Test
+    public void getRefundValueFullRefund() {
+        Schedule schedule = new Schedule();
+
+        LocalDateTime startDateTime = LocalDateTime.now().plusDays(2);
+
+        schedule.setStartDateTime(startDateTime);
+
+        //Assert.assertEquals(reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()), new BigDecimal(10));
+    }
+}
