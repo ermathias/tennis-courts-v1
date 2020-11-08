1156bfe5d9b52531bd9c802b38df9cd9992b0f6c
thomas de aguiar de oliveira <thomas.aguiar@ufpel.edu.br>
2020-11-07 23:10:09 -0300

    completing functions and inserting in the swagger

--- src/main/java/com/tenniscourts/schedules/ScheduleRepository.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleRepository.java
@@ -1,10 +1,18 @@
 package com.tenniscourts.schedules;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     List<Schedule> findByTennisCourt_IdOrderByStartDateTime(Long id);
+
+    List<Schedule> findAllByStartDateTimeLessThanEqualAndEndDateTimeGreaterThanEqual(LocalDateTime endDate,
+            LocalDateTime startDate);
+
+    List<Schedule> findAllByStartDateTimeGreaterThanEqual(LocalDateTime startDate);
 }
