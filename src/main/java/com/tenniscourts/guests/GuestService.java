99d1d4ba2f11153c24de822c897b01e2497e75a5
gustavo-gallarreta <ggfdo7@gmail.com>
2021-01-26 16:33:26 -0300

    feat(GuestService):created GuestService.

---
+++ src/main/java/com/tenniscourts/guests/GuestService.java
@@ -0,0 +1,22 @@
+package com.tenniscourts.guests;
+
+import com.tenniscourts.exceptions.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class GuestService {
+
+    private final GuestRepository guestRepository;
+
+    private final GuestMapper guestMapper;
+
+    public GuestDTO addGuest(GuestDTO guest) {
+        return guestMapper.map(guestRepository.saveAndFlush(guestMapper.map(guest)));
+    }
+
+    public GuestDTO findGuestById(Long id) {
+        return guestRepository.findById(id).map(guestMapper::map).orElseThrow(() -> new EntityNotFoundException("Guest not found."));
+    }
+}
