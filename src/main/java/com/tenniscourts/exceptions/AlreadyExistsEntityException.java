c327969c5a3bb5e2ae8f3ee4dc00b00e570ab57c
gustavo-gallarreta <ggfdo7@gmail.com>
2021-01-27 17:06:48 -0300

    fix(Indent): indent fixed.

--- src/main/java/com/tenniscourts/exceptions/AlreadyExistsEntityException.java
+++ src/main/java/com/tenniscourts/exceptions/AlreadyExistsEntityException.java
@@ -4,14 +4,15 @@ package com.tenniscourts.exceptions;
  * The type Already exists entity exception.
  */
 public class AlreadyExistsEntityException extends RuntimeException {
-  /**
-   * Instantiates a new Already exists entity exception.
-   *
-   * @param msg the msg
-   */
-  public AlreadyExistsEntityException(String msg){
+    /**
+     * Instantiates a new Already exists entity exception.
+     *
+     * @param msg the msg
+     */
+    public AlreadyExistsEntityException(String msg) {
         super(msg);
     }
 
-    private AlreadyExistsEntityException(){}
+    private AlreadyExistsEntityException() {
+    }
 }
