8b05723240dfeb61d62e31abd4164a4b43e2dfc6
gustavo-gallarreta <ggfdo7@gmail.com>
2021-02-03 14:49:41 -0300

    feat(TennisCourtController): added Swagger annotations and responses.

--- src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
+++ src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
@@ -1,30 +1,44 @@
 package com.tenniscourts.tenniscourts;
 
 import com.tenniscourts.config.BaseRestController;
-import lombok.AllArgsConstructor;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
 @RestController
-@RequestMapping(value = "/api/tenniscourt")
+@RequestMapping(value = "/v1/courts")
+@CrossOrigin(origins = "*")
 public class TennisCourtController extends BaseRestController {
-    @Autowired
+
     private final TennisCourtService tennisCourtService;
 
-    @PostMapping(value = "/add")
+    @Autowired
+    public TennisCourtController(TennisCourtService tennisCourtService) {
+        this.tennisCourtService = tennisCourtService;
+    }
+
+    @PostMapping
+    @ApiOperation(value = "API operation that create a new tennis court.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Tennis court successfully created.")
+    })
     public ResponseEntity<Void> addTennisCourt(@RequestBody TennisCourtDTO tennisCourtDTO) {
-        return ResponseEntity.created(locationByEntity(tennisCourtService.addTennisCourt(tennisCourtDTO).getId())).build();
+        return ResponseEntity.created(locationByEntity(
+                tennisCourtService.addTennisCourt(tennisCourtDTO).getId()))
+                .build();
     }
 
-    @GetMapping(value = "/findcourt/{tennisCourtId}")
-    public ResponseEntity<TennisCourtDTO> findTennisCourtById(@PathVariable Long tennisCourtId) {
-        return ResponseEntity.ok(tennisCourtService.findTennisCourtById(tennisCourtId));
+    @GetMapping("/{courtId}")
+    @ApiOperation(value = "API operation that return a tennis court by ID number.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Tennis court found."),
+            @ApiResponse(code = 404, message = "Tennis court not found.")
+    })
+    public ResponseEntity<TennisCourtDTO> findGuestById(@PathVariable Long courtId) {
+        return ResponseEntity.ok(tennisCourtService.findTennisCourtById(courtId));
     }
 
-    @GetMapping(value = "/findcourtscheduled{tennisCourtId}")
-    public ResponseEntity<TennisCourtDTO> findTennisCourtWithSchedulesById(@PathVariable Long tennisCourtId) {
-        return ResponseEntity.ok(tennisCourtService.findTennisCourtWithSchedulesById(tennisCourtId));
-    }
 }
