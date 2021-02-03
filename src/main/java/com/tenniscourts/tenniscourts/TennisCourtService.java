16ff165bb1899d95f97891f2dd4fc0f7f7a89e5e
gustavo-gallarreta <ggfdo7@gmail.com>
2021-02-03 14:47:26 -0300

    feat(TennisCourtService): refactored class.

--- src/main/java/com/tenniscourts/tenniscourts/TennisCourtService.java
+++ src/main/java/com/tenniscourts/tenniscourts/TennisCourtService.java
@@ -1,7 +1,6 @@
 package com.tenniscourts.tenniscourts;
 
 import com.tenniscourts.exceptions.EntityNotFoundException;
-import com.tenniscourts.schedules.ScheduleService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +10,6 @@ public class TennisCourtService {
 
     private final TennisCourtRepository tennisCourtRepository;
 
-    private final ScheduleService scheduleService;
-
     private final TennisCourtMapper tennisCourtMapper;
 
     public TennisCourtDTO addTennisCourt(TennisCourtDTO tennisCourt) {
@@ -20,12 +17,12 @@ public class TennisCourtService {
     }
 
     public TennisCourtDTO findTennisCourtById(Long id) {
-        return tennisCourtRepository.findById(id).map(tennisCourtMapper::map).orElseThrow(() -> new EntityNotFoundException("Tennis Court not found."));
+        return tennisCourtRepository.findById(id).map(tennisCourtMapper::map).orElseThrow(() -> throwEntityNotFoundException(id));
     }
 
-    public TennisCourtDTO findTennisCourtWithSchedulesById(Long tennisCourtId) {
-        TennisCourtDTO tennisCourtDTO = findTennisCourtById(tennisCourtId);
-        tennisCourtDTO.setTennisCourtSchedules(scheduleService.findSchedulesByTennisCourtId(tennisCourtId));
-        return tennisCourtDTO;
+    private EntityNotFoundException throwEntityNotFoundException(Object court) {
+        return new EntityNotFoundException(String.format("Court %s not found.", court));
     }
+
 }
+
