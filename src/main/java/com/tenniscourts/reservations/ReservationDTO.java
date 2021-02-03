0f0809cd151fdddc3704b7bbc68a9f0ecdc6cd53
gustavo-gallarreta <ggfdo7@gmail.com>
2021-02-03 15:10:07 -0300

    feat(ReservationDTO): added parameters.

--- src/main/java/com/tenniscourts/reservations/ReservationDTO.java
+++ src/main/java/com/tenniscourts/reservations/ReservationDTO.java
@@ -1,10 +1,12 @@
 package com.tenniscourts.reservations;
 
+import com.tenniscourts.guests.GuestDTO;
 import com.tenniscourts.schedules.ScheduleDTO;
 import lombok.*;
 
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,6 +19,8 @@ public class ReservationDTO {
     private Long id;
 
     private ScheduleDTO schedule;
+
+    private GuestDTO guest;
 
     private String reservationStatus;
 
@@ -31,4 +35,11 @@ public class ReservationDTO {
 
     @NotNull
     private Long guestId;
+
+    @NotNull
+    private LocalDateTime startDateTime;
+
+    @NotNull
+    private LocalDateTime endDateTime;
+
 }
