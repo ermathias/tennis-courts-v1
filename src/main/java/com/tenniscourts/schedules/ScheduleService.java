95e1b3814d4f0633a9cc4c2689e3ebc266478256
gustavo-gallarreta <ggfdo7@gmail.com>
2021-02-03 14:28:41 -0300

    feat(ScheduleService): added methods.

--- src/main/java/com/tenniscourts/schedules/ScheduleService.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleService.java
@@ -1,35 +1,76 @@
 package com.tenniscourts.schedules;
 
-import lombok.AllArgsConstructor;
+import com.tenniscourts.exceptions.BusinessException;
+import com.tenniscourts.exceptions.EntityNotFoundException;
+import com.tenniscourts.tenniscourts.TennisCourtService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
-@AllArgsConstructor
 public class ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
 
     private final ScheduleMapper scheduleMapper;
 
-    public ScheduleDTO addSchedule(Long tennisCourtId, CreateScheduleRequestDTO createScheduleRequestDTO) {
-        //TODO: implement addSchedule
-        return null;
+    private final TennisCourtService tennisCourtService;
+
+    @Autowired
+    public ScheduleService(ScheduleRepository scheduleRepository, ScheduleMapper scheduleMapper,
+                           TennisCourtService tennisCourtService) {
+        this.scheduleRepository = scheduleRepository;
+        this.scheduleMapper = scheduleMapper;
+        this.tennisCourtService = tennisCourtService;
     }
 
-    public List<ScheduleDTO> findSchedulesByDates(LocalDateTime startDate, LocalDateTime endDate) {
-        //TODO: implement
-        return null;
+    public ScheduleDTO addSchedule(CreateScheduleRequestDTO createScheduleRequestDTO) {
+        ScheduleDTO scheduleDTO = createScheduleDTO(createScheduleRequestDTO);
+
+        checkAddSchedule(scheduleDTO);
+
+        return scheduleMapper.map(scheduleRepository.saveAndFlush(scheduleMapper.map(scheduleDTO)));
     }
 
-    public ScheduleDTO findSchedule(Long scheduleId) {
-        //TODO: implement
-        return null;
+    private ScheduleDTO createScheduleDTO(CreateScheduleRequestDTO createScheduleRequestDTO) {
+        return ScheduleDTO.builder()
+                .startDateTime(createScheduleRequestDTO.getStartDateTime())
+                .endDateTime(createScheduleRequestDTO.getEndDateTime())
+                .tennisCourtId(createScheduleRequestDTO.getTennisCourtId())
+                .tennisCourt(tennisCourtService.findTennisCourtById(createScheduleRequestDTO.getTennisCourtId()))
+                .build();
     }
 
-    public List<ScheduleDTO> findSchedulesByTennisCourtId(Long tennisCourtId) {
-        return scheduleMapper.map(scheduleRepository.findByTennisCourt_IdOrderByStartDateTime(tennisCourtId));
+    public ScheduleDTO findScheduleById(Long scheduleId) {
+        return scheduleRepository.findById(scheduleId)
+                .map(scheduleMapper::map)
+                .orElseThrow(() -> throwEntityNotFoundException(scheduleId));
     }
+
+    public List<ScheduleDTO> findSchedulesByTennisCourtId(Long courtId) {
+        return scheduleMapper.map(scheduleRepository.findByTennisCourtIdOrderByStartDateTime(courtId));
+    }
+
+    private EntityNotFoundException throwEntityNotFoundException(Object schedule) {
+        return new EntityNotFoundException(String.format("Schedule %s not found.", schedule));
+    }
+
+    private void checkAddSchedule(ScheduleDTO scheduleDTO) {
+        LocalDateTime startDateTime = scheduleDTO.getStartDateTime();
+        LocalDateTime endDateTime = scheduleDTO.getEndDateTime();
+
+        if (startDateTime.isBefore(LocalDateTime.now())) {
+            throwBusinessException("The schedule cannot be before from local date time.");
+        }
+        if (endDateTime.isBefore(startDateTime)) {
+            throwBusinessException("The schedule cannot be before from start date time.");
+        }
+    }
+
+    private void throwBusinessException(String message) {
+        throw new BusinessException(message);
+    }
+
 }
