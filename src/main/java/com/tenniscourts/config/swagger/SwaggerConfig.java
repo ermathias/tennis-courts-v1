f6790aa02f01c246ac405a7d87a1dd20c952dd35
Suelem Kleinkauf <suelem.kleinkauf@meta.com.br>
2021-03-16 13:01:47 -0300

    feat(swagger): Add swagger configs

--- src/main/java/com/tenniscourts/config/swagger/SwaggerConfig.java
+++ src/main/java/com/tenniscourts/config/swagger/SwaggerConfig.java
@@ -2,14 +2,18 @@ package com.tenniscourts.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket productApi() {
@@ -17,5 +21,23 @@ public class SwaggerConfig {
                 .select().apis(RequestHandlerSelectors.basePackage("com.tenniscourts"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("\"Spring Boot REST API for greeting people\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
