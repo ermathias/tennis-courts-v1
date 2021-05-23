5cf783a615977963d40321fa452989ec5ffa094a
cbittenc <cristian.candia7@gmail.com>
2021-05-23 14:19:30 -0300

    Implementations made in TennisCourtController

--- src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
+++ src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
@@ -1,6 +1,7 @@
 package com.tenniscourts.tenniscourts;
 
 import com.tenniscourts.config.BaseRestController;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,18 +17,19 @@ public class TennisCourtController extends BaseRestController {
     @Autowired
     private final TennisCourtService tennisCourtService;
 
-    //TODO: implement swagger
+    @ApiOperation("Adds a tennis court to the database")
     @PostMapping("add")
     public ResponseEntity<Void> addTennisCourt(@RequestBody TennisCourtDTO tennisCourtDTO) {
         return ResponseEntity.created(locationByEntity(tennisCourtService.addTennisCourt(tennisCourtDTO).getId())).build();
     }
-
+    @ApiOperation("Get all the tennis courts registered in the database")
     @GetMapping
     public ResponseEntity<List<TennisCourt>> getAllTennisCourts() {
         return ResponseEntity.ok(tennisCourtService.getAllTennisCourts());
     }
 
-    //TODO: implement rest and swagger
+    @ApiOperation("Get a tennis court by id")
+    @GetMapping
     public ResponseEntity<TennisCourtDTO> findTennisCourtById(Long tennisCourtId) {
         return ResponseEntity.ok(tennisCourtService.findTennisCourtById(tennisCourtId));
     }
