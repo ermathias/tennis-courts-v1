60d36265f53348365dbb6a57cbdbce352aa5c964
cbittenc <cristian.candia7@gmail.com>
2021-05-24 05:44:23 -0300

    add unit test for GuestService

--- src/main/java/com/tenniscourts/schedules/CreateScheduleRequestDTO.java
+++ src/main/java/com/tenniscourts/schedules/CreateScheduleRequestDTO.java
@@ -14,7 +14,7 @@ public class CreateScheduleRequestDTO {
     @NotNull
     private Long tennisCourtId;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm")
     @NotNull
     private LocalDateTime startDateTime;
 
