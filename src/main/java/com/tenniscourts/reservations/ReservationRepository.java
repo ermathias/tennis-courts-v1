a8b4e761c1639a300c5383d2f4edd3070607b46d
brian <brian@DESKTOP-OU20I5A.attlocal.net>
2021-06-03 17:47:07 -0400

    Implemented bookReservation method.

--- src/main/java/com/tenniscourts/reservations/ReservationRepository.java
+++ src/main/java/com/tenniscourts/reservations/ReservationRepository.java
@@ -10,6 +10,8 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     List<Reservation> findBySchedule_Id(Long scheduleId);
 
     List<Reservation> findByReservationStatusAndSchedule_StartDateTimeGreaterThanEqualAndSchedule_EndDateTimeLessThanEqual(ReservationStatus reservationStatus, LocalDateTime startDateTime, LocalDateTime endDateTime);
+    
+    void createReservation(CreateReservationRequestDTO createReservationRequestDTO);
 
 //    List<Reservation> findByStartDateTimeGreaterThanEqualAndEndDateTimeLessThanEqualAndTennisCourt(LocalDateTime startDateTime, LocalDateTime endDateTime, TennisCourt tennisCourt);
 }
