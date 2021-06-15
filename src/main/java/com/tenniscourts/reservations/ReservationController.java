3d60c74adaf23a9450a453c1dd76c5fc37fe6e32
Dan Indre <indre.dan96@gmail.com>
2021-06-15 22:55:32 +0300

    feat: Add Swagger annotations to ReservationController

--- src/main/java/com/tenniscourts/reservations/ReservationController.java
+++ src/main/java/com/tenniscourts/reservations/ReservationController.java
@@ -3,10 +3,12 @@ package com.tenniscourts.reservations;
 import com.tenniscourts.UriConstants;
 import com.tenniscourts.config.BaseRestController;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.glassfish.jersey.server.Uri;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,21 +23,30 @@ public class ReservationController extends BaseRestController {
     private final ReservationService reservationService;
 
     @PostMapping(path = UriConstants.BOOK_PATH)
+    @ApiOperation("Book a new reservation")
+    @ApiResponses(value = { @ApiResponse(code = 201, message = "Reservation booked successfully") })
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Void> bookReservation(@RequestBody @Valid CreateReservationRequestDTO createReservationRequestDTO) {
         return ResponseEntity.created(locationByEntity(reservationService.bookReservation(createReservationRequestDTO).getId())).build();
     }
 
     @GetMapping(path = UriConstants.RESERVATION_ID_VARIABLE)
+    @ApiOperation("Find an existing reservation by id")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK") })
     public ResponseEntity<ReservationDTO> findReservation(@PathVariable Long reservationId) {
         return ResponseEntity.ok(reservationService.findReservation(reservationId));
     }
 
     @DeleteMapping(path = UriConstants.RESERVATION_ID_VARIABLE)
+    @ApiOperation("Cancel an existing reservation by id")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Reservation cancelled successfully") })
     public ResponseEntity<ReservationDTO> cancelReservation(@PathVariable Long reservationId) {
         return ResponseEntity.ok(reservationService.cancelReservation(reservationId));
     }
 
     @PutMapping(path = UriConstants.RESCHEDULE_PATH + UriConstants.RESERVATION_ID_VARIABLE + UriConstants.SCHEDULE_ID_VARIABLE)
+    @ApiOperation("Reschedule an existing reservation by id and scheduleId")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Reservation rescheduled successfully") })
     public ResponseEntity<ReservationDTO> rescheduleReservation(@PathVariable Long reservationId, @PathVariable Long scheduleId) {
         return ResponseEntity.ok(reservationService.rescheduleReservation(reservationId, scheduleId));
     }
