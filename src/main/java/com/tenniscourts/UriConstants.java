c247b9d22e0555734974e61f783085f86d14758a
Dan Indre <indre.dan96@gmail.com>
2021-06-15 22:41:32 +0300

    feat: Add UriConstants to keep the paths separated

---
+++ src/main/java/com/tenniscourts/UriConstants.java
@@ -0,0 +1,14 @@
+package com.tenniscourts;
+
+public final class UriConstants
+{
+    public static final String BOOK_PATH = "/book";
+
+    public static final String RESCHEDULE_PATH = "/reschedule";
+
+    public static final String RESERVATION_PATH = "/reservation";
+
+    public static final String RESERVATION_ID_VARIABLE = "/{reservationId}";
+
+    public static final String SCHEDULE_ID_VARIABLE = "/{scheduleId}";
+}
