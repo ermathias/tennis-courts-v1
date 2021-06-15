a2d0fc17b9aae8c3bc62af51b8860799d0e44679
Dan Indre <indre.dan96@gmail.com>
2021-06-15 22:42:01 +0300

    feat: Add REST annotations for ReservationController

--- src/main/java/com/tenniscourts/reservations/ReservationController.java
+++ src/main/java/com/tenniscourts/reservations/ReservationController.java
@@ -1,27 +1,42 @@
 package com.tenniscourts.reservations;
 
+import com.tenniscourts.UriConstants;
 import com.tenniscourts.config.BaseRestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
+import org.glassfish.jersey.server.Uri;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @AllArgsConstructor
+@RestController
+@RequestMapping(path = UriConstants.RESERVATION_PATH)
 public class ReservationController extends BaseRestController {
 
     private final ReservationService reservationService;
 
-    public ResponseEntity<Void> bookReservation(CreateReservationRequestDTO createReservationRequestDTO) {
+    @PostMapping(path = UriConstants.BOOK_PATH)
+    public ResponseEntity<Void> bookReservation(@RequestBody @Valid CreateReservationRequestDTO createReservationRequestDTO) {
         return ResponseEntity.created(locationByEntity(reservationService.bookReservation(createReservationRequestDTO).getId())).build();
     }
 
-    public ResponseEntity<ReservationDTO> findReservation(Long reservationId) {
+    @GetMapping(path = UriConstants.RESERVATION_ID_VARIABLE)
+    public ResponseEntity<ReservationDTO> findReservation(@PathVariable Long reservationId) {
         return ResponseEntity.ok(reservationService.findReservation(reservationId));
     }
 
-    public ResponseEntity<ReservationDTO> cancelReservation(Long reservationId) {
+    @DeleteMapping(path = UriConstants.RESERVATION_ID_VARIABLE)
+    public ResponseEntity<ReservationDTO> cancelReservation(@PathVariable Long reservationId) {
         return ResponseEntity.ok(reservationService.cancelReservation(reservationId));
     }
 
-    public ResponseEntity<ReservationDTO> rescheduleReservation(Long reservationId, Long scheduleId) {
+    @PutMapping(path = UriConstants.RESCHEDULE_PATH + UriConstants.RESERVATION_ID_VARIABLE + UriConstants.SCHEDULE_ID_VARIABLE)
+    public ResponseEntity<ReservationDTO> rescheduleReservation(@PathVariable Long reservationId, @PathVariable Long scheduleId) {
         return ResponseEntity.ok(reservationService.rescheduleReservation(reservationId, scheduleId));
     }
 }
