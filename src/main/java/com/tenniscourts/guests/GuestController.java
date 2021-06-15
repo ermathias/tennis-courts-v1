af5adb7d04d3e2e34b49727993737b74c6fea474
Dan Indre <indre.dan96@gmail.com>
2021-06-16 00:52:33 +0300

    feat: Implementation for guest methods

--- src/main/java/com/tenniscourts/guests/GuestController.java
+++ src/main/java/com/tenniscourts/guests/GuestController.java
@@ -6,7 +6,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
-import org.glassfish.jersey.server.Uri;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,25 +18,27 @@ import java.util.List;
 @RequestMapping(path = UriConstants.GUEST_PATH)
 public class GuestController extends BaseRestController {
 
+    private final GuestService guestService;
+
     @GetMapping(path = UriConstants.ALL_PATH)
     @ApiOperation("Find all guests")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK") })
     public ResponseEntity<List<GuestDTO>> findAllGuests() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(this.guestService.handleFindAllGuests());
     }
 
     @GetMapping(path = UriConstants.GUEST_ID_VARIABLE)
     @ApiOperation("Find an existing guest by id")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK") })
-    public ResponseEntity<GuestDTO> findGuestById(@PathVariable String guestId) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<GuestDTO> findGuestById(@PathVariable Long guestId) {
+        return ResponseEntity.ok(this.guestService.handleFindGuestById(guestId));
     }
 
     @GetMapping(path = UriConstants.NAME_PATH + UriConstants.GUEST_NAME_VARIABLE)
     @ApiOperation("Find an existing guest by name")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK") })
     public ResponseEntity<List<GuestDTO>> findGuestByName(@PathVariable String guestName) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(this.guestService.handleFindGuestsByName(guestName));
     }
 
     @PostMapping
@@ -45,20 +46,20 @@ public class GuestController extends BaseRestController {
     @ApiResponses(value = { @ApiResponse(code = 201, message = "Guest created successfully") })
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Void> createGuest(@RequestBody @Valid CreateGuestRequestDTO createGuestRequestDTO) {
-        return ResponseEntity.created(locationByEntity(1L)).build();
+        return ResponseEntity.created(locationByEntity(this.guestService.handleCreateGuest(createGuestRequestDTO).getId())).build();
     }
 
     @PutMapping(path = UriConstants.GUEST_ID_VARIABLE + UriConstants.GUEST_NAME_VARIABLE)
     @ApiOperation("Update an existing guest")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "Guest updated successfully") })
-    public ResponseEntity<Void> updateGuest(@PathVariable Long guestId, @PathVariable String guestName) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<GuestDTO> updateGuest(@PathVariable Long guestId, @PathVariable String guestName) {
+        return ResponseEntity.ok(this.guestService.handleUpdateGuest(guestId, guestName));
     }
 
     @DeleteMapping(path = UriConstants.GUEST_ID_VARIABLE)
     @ApiOperation("Delete an existing guest")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "Guest deleted successfully") })
-    public ResponseEntity<Void> deleteGuest(@PathVariable Long guestId) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Long> deleteGuest(@PathVariable Long guestId) {
+        return ResponseEntity.ok(this.guestService.handleDeleteGuest(guestId));
     }
 }
