0bcf039e58a7cca451b3c23ef7bde865eef34462
Dan Indre <indre.dan96@gmail.com>
2021-06-16 00:21:10 +0300

    feat: Create GuestDTO, CreateGuestRequestDTO and GuestMapper

---
+++ src/main/java/com/tenniscourts/guests/CreateGuestRequestDTO.java
@@ -0,0 +1,16 @@
+package com.tenniscourts.guests;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Data
+public class CreateGuestRequestDTO {
+    @NotNull
+    private String name;
+}
---
+++ src/main/java/com/tenniscourts/guests/GuestMapper.java
@@ -0,0 +1,9 @@
+package com.tenniscourts.guests;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface GuestMapper {
+    Guest map(GuestDTO source);
+
+}
