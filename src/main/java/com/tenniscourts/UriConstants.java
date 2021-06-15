0ef4847c7b352a101767fb226a2e4aae31774081
Dan Indre <indre.dan96@gmail.com>
2021-06-16 00:36:14 +0300

    feat: Add more path constants for user

--- src/main/java/com/tenniscourts/UriConstants.java
+++ src/main/java/com/tenniscourts/UriConstants.java
@@ -2,11 +2,26 @@ package com.tenniscourts;
 
 public final class UriConstants
 {
+    private UriConstants()
+    {
+        // Private constructor for constants class
+    }
+
+    public static final String ALL_PATH = "/all";
+
     public static final String BOOK_PATH = "/book";
+
+    public static final String GUEST_PATH = "/guest";
+
+    public static final String NAME_PATH = "/name";
 
     public static final String RESCHEDULE_PATH = "/reschedule";
 
     public static final String RESERVATION_PATH = "/reservation";
+
+    public static final String GUEST_ID_VARIABLE = "/{guestId}";
+
+    public static final String GUEST_NAME_VARIABLE = "/{guestName}";
 
     public static final String RESERVATION_ID_VARIABLE = "/{reservationId}";
 
