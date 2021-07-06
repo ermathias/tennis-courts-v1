a36394dc5f99b0b4240b4fc66f82854759f4a10b
Tiago Veri <tiagoverisjc@gmail.com>
2021-07-06 16:51:23 -0300

    Guest CRUD created

---
+++ src/main/java/com/tenniscourts/guests/GuestRepository.java
@@ -0,0 +1,10 @@
+package com.tenniscourts.guests;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+
+      Guest findByName(String guestName);
+}
