0bbbfee9d1efab75cc5b3c796b3929b76757f83a
Gromowlad <perungromowlad@gmail.com>
2021-07-07 22:00:27 +0100

    Added REST and Swagger for TennisCourtController and ScheduleController

--- src/main/java/com/tenniscourts/schedules/ScheduleController.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleController.java
@@ -1,31 +1,47 @@
 package com.tenniscourts.schedules;
 
 import com.tenniscourts.config.BaseRestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
+@Api("Schedule")
 @AllArgsConstructor
+@RestController
+@RequestMapping("/schedule")
 public class ScheduleController extends BaseRestController {
 
     private final ScheduleService scheduleService;
 
-    //TODO: implement rest and swagger
+    @PostMapping(value = "/add")
+    @ApiOperation(value="Add a schedule")
+    @ApiResponse(code = 201, message = "Schedule created")
     public ResponseEntity<Void> addScheduleTennisCourt(CreateScheduleRequestDTO createScheduleRequestDTO) {
         return ResponseEntity.created(locationByEntity(scheduleService.addSchedule(createScheduleRequestDTO.getTennisCourtId(), createScheduleRequestDTO).getId())).build();
     }
 
-    //TODO: implement rest and swagger
+    @GetMapping(value = "/find", params = {"startDate", "endDate"})
+    @ApiOperation(value="Find a schedule by a date range - start and end date")
+    @ApiResponse(code = 200, message = "Schedule found")
     public ResponseEntity<List<ScheduleDTO>> findSchedulesByDates(LocalDate startDate,
                                                                   LocalDate endDate) {
         return ResponseEntity.ok(scheduleService.findSchedulesByDates(LocalDateTime.of(startDate, LocalTime.of(0, 0)), LocalDateTime.of(endDate, LocalTime.of(23, 59))));
     }
 
-    //TODO: implement rest and swagger
+    @GetMapping(value = "/find", params = "scheduleId")
+    @ApiOperation(value="Find a schedule by ID")
+    @ApiResponse(code = 200, message = "Schedule found")
     public ResponseEntity<ScheduleDTO> findByScheduleId(Long scheduleId) {
         return ResponseEntity.ok(scheduleService.findSchedule(scheduleId));
     }
--- src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
+++ src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
@@ -1,8 +1,18 @@
 package com.tenniscourts.tenniscourts;
 
 import com.tenniscourts.config.BaseRestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Api("Entry Point")
 @AllArgsConstructor
@@ -12,17 +22,23 @@ public class TennisCourtController extends BaseRestController {
 
     private final TennisCourtService tennisCourtService;
 
-    //TODO: implement rest and swagger
-    public ResponseEntity<Void> addTennisCourt(TennisCourtDTO tennisCourtDTO) {
+    @PostMapping(value = "/add")
+    @ApiOperation(value="Add Tennis Court")
+    @ApiResponse(code = 201, message = "Tennis Court created")
+    public ResponseEntity<Void> addTennisCourt(@Valid TennisCourtDTO tennisCourtDTO) {
         return ResponseEntity.created(locationByEntity(tennisCourtService.addTennisCourt(tennisCourtDTO).getId())).build();
     }
 
-    //TODO: implement rest and swagger
+    @GetMapping(value = "/find", params = "tennisCourtId", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value="Find Tennis Court by ID")
+    @ApiResponse(code = 200, message = "Tennis Court found")
     public ResponseEntity<TennisCourtDTO> findTennisCourtById(Long tennisCourtId) {
         return ResponseEntity.ok(tennisCourtService.findTennisCourtById(tennisCourtId));
     }
 
-    //TODO: implement rest and swagger
+    @GetMapping(value = "/find-with-schedule", params = "tennisCourtId", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value="Find with schedule")
+    @ApiResponse(code = 200, message = "Tennis Court with schedule found")
     public ResponseEntity<TennisCourtDTO> findTennisCourtWithSchedulesById(Long tennisCourtId) {
         return ResponseEntity.ok(tennisCourtService.findTennisCourtWithSchedulesById(tennisCourtId));
     }
