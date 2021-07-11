11a37968d6c396e74f4c57cf96ecd04601c37ace
Ricardo Ribeiro <riceirolves@gmail.com>
2021-07-11 16:20:26 -0300

    create and retrieve endpoints added

--- src/main/java/com/tenniscourts/guests/GuestController.java
+++ src/main/java/com/tenniscourts/guests/GuestController.java
@@ -1,8 +1,5 @@
 package com.tenniscourts.guests;
 
-import com.tenniscourts.config.BaseRestController;
-import lombok.AllArgsConstructor;
-
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -11,19 +8,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tenniscourts.config.BaseRestController;
+
+import lombok.AllArgsConstructor;
+
 @RestController
 @AllArgsConstructor
 public class GuestController extends BaseRestController {
 
-    private final GuestService guestService;
+	private final GuestService guestService;
 
-    @PostMapping(value = "/v1/guest/create")
-    public ResponseEntity<Void> createGuest(@RequestBody CreateGuestRequestDTO createReservationRequestDTO) {
-        return ResponseEntity.created(locationByEntity(guestService.createGuest(createReservationRequestDTO).getId())).build();
-    }
+	@PostMapping(value = "/v1/guest/create")
+	public ResponseEntity<Void> createGuest(@RequestBody GuestDTO guestDTO) {
+		return ResponseEntity.created(locationByEntity(guestService.createGuest(guestDTO).getId())).build();
+	}
 
-    @GetMapping(value = "/v1/guest/retrieve")
-    public ResponseEntity<List<GuestDTO>> findGuests() {
-        return ResponseEntity.ok(guestService.findGuests());
-    }
+	@GetMapping(value = "/v1/guest/retrieve")
+	public ResponseEntity<List<GuestDTO>> findGuests() {
+		return ResponseEntity.ok(guestService.findGuests());
+	}
 }
