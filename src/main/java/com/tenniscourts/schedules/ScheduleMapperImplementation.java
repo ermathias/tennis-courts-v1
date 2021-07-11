22337bee07d000a86a1a2b818ab65106ef56c2ea
Ricardo Ribeiro <riceirolves@gmail.com>
2021-07-11 11:59:59 -0300

    implementations added

---
+++ src/main/java/com/tenniscourts/schedules/ScheduleMapperImplementation.java
@@ -0,0 +1,28 @@
+package com.tenniscourts.schedules;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduleMapperImplementation implements ScheduleMapper{
+
+	@Override
+	public Schedule map(ScheduleDTO source) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ScheduleDTO map(Schedule source) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ScheduleDTO> map(List<Schedule> source) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
