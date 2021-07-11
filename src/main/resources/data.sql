e2f5e0f214cdbbeac6c29b1c51f1a4cab64e9034
Ricardo Ribeiro <riceirolves@gmail.com>
2021-07-11 19:32:12 -0300

    added two more schedules

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,12 @@ insert into schedule (id, start_date_time, end_date_time, tennis_court_id)
 values (null, '2020-01-10T20:00:00.0', '2020-02-10T21:00:00.0', 1);
 
 insert into schedule (id, start_date_time, end_date_time, tennis_court_id)
+values (null, '2020-04-10T20:00:00.0', '2020-09-10T21:00:00.0', 1);
+
+insert into schedule (id, start_date_time, end_date_time, tennis_court_id)
+values (null, '2021-03-10T20:00:00.0', '2021-07-10T21:00:00.0', 1);
+
+insert into schedule (id, start_date_time, end_date_time, tennis_court_id)
 values (null, '2020-02-15T20:00:00.0', '2020-03-30T21:00:00.0', 2);
 
 insert into schedule (id, start_date_time, end_date_time, tennis_court_id)
