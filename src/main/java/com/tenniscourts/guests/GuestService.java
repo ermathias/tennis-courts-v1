5156c857817a9c4e6dd2690b1ac4d4dfd6c914a8
Ricardo Ribeiro <riceirolves@gmail.com>
2021-07-11 16:25:18 -0300

    createGuest and retrieveGuests added to service

--- src/main/java/com/tenniscourts/guests/GuestService.java
+++ src/main/java/com/tenniscourts/guests/GuestService.java
@@ -3,6 +3,7 @@ package com.tenniscourts.guests;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -13,14 +14,19 @@ public class GuestService {
 
 	private final GuestRepository guestRepository;
 
-	private final GuestMapper guestMapper;
+	private ModelMapper modelmapper = new ModelMapper();
 
-	public GuestDTO createGuest(CreateGuestRequestDTO createGuestRequestDTO) {
-		throw new UnsupportedOperationException();
+	public GuestDTO createGuest(GuestDTO guestDTO) {
+
+		Guest guest = guestRepository.save(modelmapper.map(guestDTO, Guest.class));
+
+		return modelmapper.map(guest, GuestDTO.class);
+
 	}
 
 	public List<GuestDTO> findGuests() {
-		return guestRepository.findAll().stream().map(guestMapper::map).collect(Collectors.toList());
+		return guestRepository.findAll().stream().map(guest -> modelmapper.map(guest, GuestDTO.class))
+				.collect(Collectors.toList());
 	}
 
 }
