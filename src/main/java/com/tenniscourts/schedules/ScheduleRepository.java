49ad1aec03650e129b946457652a3efcf479e18d
Ricardo Ribeiro <riceirolves@gmail.com>
2021-07-11 19:28:52 -0300

    findSchedulesByDates feature added

--- src/main/java/com/tenniscourts/schedules/ScheduleRepository.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleRepository.java
@@ -1,10 +1,14 @@
 package com.tenniscourts.schedules;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    List<Schedule> findByTennisCourt_IdOrderByStartDateTime(Long id);
+	List<Schedule> findByTennisCourt_IdOrderByStartDateTime(Long id);
+
+	List<Schedule> findByStartDateTimeBetweenOrEndDateTimeBetween(LocalDateTime startDate1, LocalDateTime endDate1,
+			LocalDateTime startDate2, LocalDateTime endDate2);
 }
--- src/main/java/com/tenniscourts/schedules/ScheduleService.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleService.java
@@ -1,35 +1,38 @@
 package com.tenniscourts.schedules;
 
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class ScheduleService {
 
-    private final ScheduleRepository scheduleRepository;
+	private final ScheduleRepository scheduleRepository;
 
-    private final ScheduleMapper scheduleMapper;
+	private ModelMapper mapper = new ModelMapper();
 
-    public ScheduleDTO addSchedule(Long tennisCourtId, CreateScheduleRequestDTO createScheduleRequestDTO) {
-        //TODO: implement addSchedule
-        return null;
-    }
+	public ScheduleDTO addSchedule(Long tennisCourtId, CreateScheduleRequestDTO createScheduleRequestDTO) {
+		// TODO: implement addSchedule
+		return null;
+	}
 
-    public List<ScheduleDTO> findSchedulesByDates(LocalDateTime startDate, LocalDateTime endDate) {
-        //TODO: implement
-        return null;
-    }
+	public List<ScheduleDTO> findSchedulesByDates(LocalDateTime startDate, LocalDateTime endDate) {
+		return scheduleRepository.findByStartDateTimeBetweenOrEndDateTimeBetween(startDate, endDate, startDate, endDate)
+				.stream().map(schedule -> mapper.map(schedule, ScheduleDTO.class)).collect(Collectors.toList());
+	}
 
-    public ScheduleDTO findSchedule(Long scheduleId) {
-        //TODO: implement
-        return null;
-    }
+	public ScheduleDTO findSchedule(Long scheduleId) {
+		// TODO: implement
+		return null;
+	}
 
-    public List<ScheduleDTO> findSchedulesByTennisCourtId(Long tennisCourtId) {
-        return scheduleMapper.map(scheduleRepository.findByTennisCourt_IdOrderByStartDateTime(tennisCourtId));
-    }
+	public List<ScheduleDTO> findSchedulesByTennisCourtId(Long tennisCourtId) {
+		return null; // scheduleMapper.map(scheduleRepository.findByTennisCourt_IdOrderByStartDateTime(tennisCourtId));
+	}
 }
